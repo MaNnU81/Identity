965dfa00c40305457498e00c72fc9493f00ca9c8
MaNnU <mannuspamm@gmail.com>
2025-06-13 13:23:43 +0200

    tre

---
+++ Identity.Api/model/Request.cs
@@ -0,0 +1,22 @@
+﻿namespace Identity.Api.model
+{
+    public class Request
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+
+        public string Url { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public string Status { get; set; }
+
+        public int UserId { get; set; }
+
+
+
+    }
+}
+
+
+// id, text , url, created at, status, user id
